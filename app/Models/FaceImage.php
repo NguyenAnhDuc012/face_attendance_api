15d9027d5b81cbded668a0dfe0b454eb5f128f3a
Quanh <quanh@gmail.com>
2025-11-07 23:51:04 +0700

    done

---
+++ app/Models/FaceImage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FaceImage extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'face_images';
+
+
+    protected $fillable = [
+        'student_id',
+        'image_path',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
