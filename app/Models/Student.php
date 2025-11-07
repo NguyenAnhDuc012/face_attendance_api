8cc40ab58e280e442528b34ea8ec1c13ead92c89
Quanh <quanh@gmail.com>
2025-11-07 23:50:29 +0700

    done

---
+++ app/Models/Student.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash; 
+
+class Student extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
+
+    protected $table = 'students';
+
+    protected $fillable = [
+        'full_name',
+        'dob',
+        'class_id',
+        'email',
+        'phone',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+    
+    public function studentClass()
+    {
+        return $this->belongsTo(StudentClass::class, 'class_id');
+    }
+
+    public function faceImages()
+    {
+        return $this->hasMany(FaceImage::class);
+    }
+
+    public function faceEmbeddings()
+    {
+        return $this->hasMany(FaceEmbedding::class);
+    }
+}
